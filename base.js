946e4452f6a3120e553261087d7704ac5f54f11d
Vitali <vitali007@tut.by>
2024-04-03 20:21:01 +0300

    base task

---
+++ base.js
@@ -0,0 +1,12 @@
+//* Всплытие
+console.log(typeof double);
+
+function double(num) {
+  return (num*2);
+}
+var double = 77;
+
+console.log(typeof double);
+//! FD всплывает выше var
+// function
+// number
